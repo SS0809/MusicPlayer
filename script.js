8183076838e14d1f9250493f67f36e31a5c58e07
Saurabh Saraswat <saurabh45215@gmail.com>
2022-10-23 23:42:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-   var a=0;
+  var a=0;
 function change()
 {
     //alert("hi");
@@ -13,13 +13,13 @@ function change()
    // console.log(a);
 }
 function bgchange(){
- const arr=["red","blue","black","green","pink","yellow","black"];
+ const arr=["red","blue","#1C2A3D","green","pink","yellow","black"];
    var i=0;
   setInterval(function () {
-  document.body.style.backgroundColor = arr[i];
+  //document.body.style.backgroundColor = arr[i];
+  document.body.style.background = "linear-gradient(110deg,  black,"+arr[i]+")";
   i++;
   if(i==6){bgchange();}
   }, 1200); 
  // console.log(i);
-  
 }
