379586649e19d52cb9b16c4ed92893cc256db5a3
Saurabh Saraswat <saurabh45215@gmail.com>
2022-10-30 13:46:04 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
-    $servername = "localhost";
+    /*$servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "music";
-  /*$servername = "db4free.net";
+    $database = "music";*/
+    $servername = "db4free.net";
     $username = "t_saurabhsara123";
     $password = "47EwB9_NXb_42fa";
-    $database = "music23";*/
+    $database = "music23";
 ?>
