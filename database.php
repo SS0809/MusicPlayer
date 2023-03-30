941ce71cfd272c0f1a80cf642390190b6cdebee9
SS0809 <saurabh45215@gmail.com>
2023-03-30 14:40:27 +0530

    Docker update

--- database.php
+++ database.php
@@ -1,8 +1,4 @@
 <?php
-    /*$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "music";*/
     $servername = "db4free.net";
     $username = "t_saurabhsara123";
     $password = "47EwB9_NXb_42fa";
